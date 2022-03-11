b46988da4c7c04cb62f61c2bd1252413ab0f8203
haybales <haleynicole126@gmail.com>
2022-03-10 20:52:00 -0700

    updated db to fix get route

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -3,9 +3,7 @@ const { notes } = require('../../db/db.json');
 
 
 router.get('/notes', (req, res) => {
-    let results = notes;
-    console.log(req.query)
-    res.json(results);
+    res.json(notes);
 });
 
 router.post('/notes', (req, res) => {
