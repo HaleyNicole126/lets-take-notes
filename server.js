88eba3f51dec0c439f369a7f4163fdb9f58b6308
haybales <haleynicole126@gmail.com>
2022-03-10 19:52:59 -0700

    modularize routes

--- server.js
+++ server.js
@@ -1,16 +1,10 @@
-const path = require('path');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+const htmlRoutes = require('./routes/htmlRoutes');
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
-});
-
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'));
-});
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
